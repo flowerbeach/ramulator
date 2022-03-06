976ece8f9d42c790c8e67cef94f50b3c93e3af9e
STDHP1 <flowerbeach@163.com>
2022-03-06 16:35:14 +0800

    refactor

--- src/Gem5Wrapper.cpp
+++ src/Gem5Wrapper.cpp
@@ -17,36 +17,36 @@
 
 using namespace ramulator;
 
-static map<string, function<MemoryBase *(const Config&, int)> > name_to_func = {
-    {"DDR3", &MemoryFactory<DDR3>::create}, {"DDR4", &MemoryFactory<DDR4>::create},
-    {"LPDDR3", &MemoryFactory<LPDDR3>::create}, {"LPDDR4", &MemoryFactory<LPDDR4>::create},
-    {"GDDR5", &MemoryFactory<GDDR5>::create}, 
-    {"WideIO", &MemoryFactory<WideIO>::create}, {"WideIO2", &MemoryFactory<WideIO2>::create},
-    {"HBM", &MemoryFactory<HBM>::create},
-    {"SALP-1", &MemoryFactory<SALP>::create}, {"SALP-2", &MemoryFactory<SALP>::create}, {"SALP-MASA", &MemoryFactory<SALP>::create},
+static map<string, function<MemoryBase *(const Config &, int)> > name_to_func = {
+        {"DDR3",      &MemoryFactory<DDR3>::create},
+        {"DDR4",      &MemoryFactory<DDR4>::create},
+        {"LPDDR3",    &MemoryFactory<LPDDR3>::create},
+        {"LPDDR4",    &MemoryFactory<LPDDR4>::create},
+        {"GDDR5",     &MemoryFactory<GDDR5>::create},
+        {"WideIO",    &MemoryFactory<WideIO>::create},
+        {"WideIO2",   &MemoryFactory<WideIO2>::create},
+        {"HBM",       &MemoryFactory<HBM>::create},
+        {"SALP-1",    &MemoryFactory<SALP>::create},
+        {"SALP-2",    &MemoryFactory<SALP>::create},
+        {"SALP-MASA", &MemoryFactory<SALP>::create},
 };
 
-
-Gem5Wrapper::Gem5Wrapper(const Config& configs, int cacheline)
-{
-    const string& std_name = configs["standard"];
+Gem5Wrapper::Gem5Wrapper(const Config &configs, int cacheline) {
+    const string &std_name = configs["standard"];
     assert(name_to_func.find(std_name) != name_to_func.end() && "unrecognized standard name");
     mem = name_to_func[std_name](configs, cacheline);
     tCK = mem->clk_ns();
 }
 
-
 Gem5Wrapper::~Gem5Wrapper() {
     delete mem;
 }
 
-void Gem5Wrapper::tick()
-{
+void Gem5Wrapper::tick() {
     mem->tick();
 }
 
-bool Gem5Wrapper::send(Request req)
-{
+bool Gem5Wrapper::send(Request req) {
     return mem->send(req);
 }
 
--- src/Gem5Wrapper.h
+++ src/Gem5Wrapper.h
@@ -7,25 +7,23 @@
 
 using namespace std;
 
-namespace ramulator
-{
-
-class Request;
-class MemoryBase;
-
-class Gem5Wrapper 
-{
-private:
-    MemoryBase *mem;
-public:
-    double tCK;
-    Gem5Wrapper(const Config& configs, int cacheline);
-    ~Gem5Wrapper();
-    void tick();
-    bool send(Request req);
-    void finish(void);
-};
-
+namespace ramulator {
+    
+    class Request;
+    class MemoryBase;
+    
+    class Gem5Wrapper {
+    private:
+        MemoryBase *mem;
+    public:
+        double tCK;
+        Gem5Wrapper(const Config &configs, int cacheline);
+        ~Gem5Wrapper();
+        void tick();
+        bool send(Request req);
+        void finish(void);
+    };
+    
 } /*namespace ramulator*/
 
 #endif /*__GEM5_WRAPPER_H*/
